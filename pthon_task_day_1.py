23c7ea633a5c0282e66687b96e130ec058304708
Sidra Jadoon <157299539+sk-jadoon@users.noreply.github.com>
2025-06-30 21:26:48 +0500

    Add files via upload

---
+++ pthon_task_day_1.py
@@ -0,0 +1,7 @@
+int = int(input("Enter the int "))
+float = float (input("Enter the float "))
+string = input("Enter the string ")
+
+print("The int number is ",  int)
+print("The floating number is ", float)
+print("The string is ", string)
